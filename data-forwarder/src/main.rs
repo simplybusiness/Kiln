9b458faad68263c5d87113ea8a13156a18900b85
Divya Muthukumaran <Divya.Muthukumaran@simplybusiness.co.uk>
2019-11-07 15:59:07 +0000

    Changed into() to to_string() for ToolReport and Environment

--- data-forwarder/src/main.rs
+++ data-forwarder/src/main.rs
@@ -1,6 +1,10 @@
+use kiln_lib::tool_report::{ToolReport, ApplicationName, GitBranch, GitCommitHash,  ToolName, ToolOutput, OutputFormat,StartTime,EndTime,Environment,ToolVersion}; 
+use kiln_lib::validation::ValidationError; 
 use clap::{Arg, App}; 
+use std::convert::TryFrom; 
+use chrono::{DateTime, Utc};
 
-fn main() {
+fn main() -> Result<(), kiln_lib::validation::ValidationError> {
     let matches = App::new("Kiln data forwarder")
 			.arg(Arg::with_name("tool_name")
 				.help("Name of the security tool run")
@@ -51,12 +55,12 @@ fn main() {
 				.value_name("OUTPUT-FORMAT")
 				.possible_values(&["JSON","Plaintext"])			
 			)
-			.arg(Arg::with_name("env")
+			.arg(Arg::with_name("scan_env")
 				.help("Environment for the tool run")
-				.long("env")
+				.long("scan-env")
 				.required(true)
 				.takes_value(true)
-				.value_name("ENV")
+				.value_name("SCANENV")
 				.possible_values(&["Local","CI"])			
 			)
 			.arg(Arg::with_name("app_name")
@@ -66,4 +70,43 @@ fn main() {
 				.takes_value(true)
 				.value_name("APPNAME")
 			).get_matches();
+
+
+	let tool_name = matches.value_of("tool_name").unwrap();
+	let tool_version = matches.value_of("tool_version"); 
+	let tool_output_path = matches.value_of("tool_output_path").unwrap();
+	let endpoint_url = matches.value_of("endpoint_url").unwrap();
+	let start_time = matches.value_of("start_time").unwrap();
+	let parsed_start_time = DateTime::parse_from_rfc3339(&start_time)
+                    .map(DateTime::<Utc>::from)
+                    .map_err(|_| ValidationError::start_time_not_a_timestamp())?;
+
+
+	let end_time = matches.value_of("end_time").unwrap();
+	let parsed_end_time = DateTime::parse_from_rfc3339(&end_time)
+                    .map(DateTime::<Utc>::from)
+                    .map_err(|_| ValidationError::end_time_not_a_timestamp())?;
+
+	let output_format = matches.value_of("output_format").unwrap();
+	let scan_env = matches.value_of("scan_env").unwrap();
+	let app_name = matches.value_of("app_name").unwrap();
+        let git_commit_hash = "70453c83913a703010dce88fdc6cb4ab1d591a81"; 
+	let git_branch_name = "master"; 
+	let tool_output = ""; 
+
+	let tool_report = ToolReport { 
+		application_name: ApplicationName::try_from(app_name.to_string())?, 
+		git_branch: GitBranch::try_from(git_branch_name.to_string())?, 
+		git_commit_hash: GitCommitHash::try_from(git_commit_hash.to_string())?, 
+		tool_name: ToolName::try_from(tool_name.to_string())?, 
+		tool_output: ToolOutput::try_from(tool_output.to_string())?, 
+		output_format: OutputFormat::try_from(output_format.to_string())?, 
+		start_time: parsed_start_time.into(), 
+		end_time: parsed_end_time.into(), 
+		environment: Environment::try_from(scan_env.to_string())?, 
+		tool_version: ToolVersion::try_from(tool_version.map(|s| s.to_string()))?, 
+	}; 	
+
+	println!("Tool Report Struct: {:?}", tool_report);  
+	Ok(())
 }
