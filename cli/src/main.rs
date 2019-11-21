e78c7716e8e7b909671a6b9a84c7af130ee38883
Daniel Murphy <daniel.murphy@simplybusiness.co.uk>
2019-11-21 10:51:05 +0000

    CLI: Add dummy command to run ruby dependencies container

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -13,4 +13,22 @@ fn main() {
                 .about("Use Bundler-audit to find known vulnerabilities in project dependencies")
             )
         ).get_matches();
+
+    let use_local_image = matches.is_present("use-local-image");
+
+    match matches.subcommand() {
+        ("ruby", Some(sub_m)) => {
+            match sub_m.subcommand_name() {
+                Some("dependencies") => {
+                    run_ruby_dependencies_tool(use_local_image);
+                },
+                _ => unreachable!()
+            }
+        },
+        _ => unreachable!()
+    }
+}
+
+fn run_ruby_dependencies_tool(use_local_image: bool) {
+    println!("{}", use_local_image)
 }
