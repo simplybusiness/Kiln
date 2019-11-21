f2280fcf04e09373631a9bebe3317ed88b86ce97
Daniel Murphy <daniel.murphy@simplybusiness.co.uk>
2019-11-21 10:15:10 +0000

    CLI: Added an intial CLI argument interface for ruby dependencies

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,13 @@
+use clap::{App, AppSettings, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("Kiln CLI")
+        .setting(AppSettings::SubcommandRequired)
+        .subcommand(SubCommand::with_name("ruby")
+            .about("perform security testing of Ruby based projects")
+            .setting(AppSettings::SubcommandRequired)
+            .subcommand(SubCommand::with_name("dependencies")
+                .about("Use Bundler-audit to find known vulnerabilities in project dependencies")
+            )
+        ).get_matches();
 }
