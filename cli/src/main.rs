0bd0122adae23a82593e543e832b2bfa771b9a17
Daniel Murphy <daniel.murphy@simplybusiness.co.uk>
2019-11-21 10:32:02 +0000

    CLI: Added flag to override default behaviour of pulling the latest tag for an image before use

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,8 +1,11 @@
-use clap::{App, AppSettings, SubCommand};
+use clap::{App, AppSettings, Arg, SubCommand};
 
 fn main() {
     let matches = App::new("Kiln CLI")
         .setting(AppSettings::SubcommandRequired)
+        .arg(Arg::with_name("use-local-image")
+            .long("use-local-image")
+            .help("Do not try and pull the latest version of a tool image. Useful for development and scanning without network access"))
         .subcommand(SubCommand::with_name("ruby")
             .about("perform security testing of Ruby based projects")
             .setting(AppSettings::SubcommandRequired)
