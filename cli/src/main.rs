27ae69e7013ff3a92a608a1c3e73040aa2ef576b
Daniel Murphy <daniel.murphy@simplybusiness.co.uk>
2019-11-22 13:58:08 +0000

    CLI: Replaced hyper and hyperlocal crates with Rust Docker client. Added logic to ensure image exists locally if request, or pull latest tag from Docker Hub if not

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,4 +1,9 @@
 use clap::{App, AppSettings, Arg, SubCommand};
+use futures::prelude::Future;
+use shiplift::{builder::PullOptions, Docker};
+use tokio::prelude::*;
+
+use std::boxed::Box;
 
 fn main() {
     let matches = App::new("Kiln CLI")
@@ -16,19 +21,66 @@ fn main() {
 
     let use_local_image = matches.is_present("use-local-image");
 
-    match matches.subcommand() {
+    let tool_fut = match matches.subcommand() {
         ("ruby", Some(sub_m)) => {
             match sub_m.subcommand_name() {
                 Some("dependencies") => {
-                    run_ruby_dependencies_tool(use_local_image);
+                    run_ruby_dependencies_tool(use_local_image)
                 },
                 _ => unreachable!()
             }
         },
         _ => unreachable!()
-    }
+    };
+
+    tokio::run(tool_fut);
 }
 
-fn run_ruby_dependencies_tool(use_local_image: bool) {
-    println!("{}", use_local_image)
+fn run_ruby_dependencies_tool(use_local_image: bool) -> Box<dyn Future<Item=(), Error=()> + Send + 'static>{
+    let tool_image_name = "kiln/bundler-audit:latest";
+    let docker = Docker::new();
+    if use_local_image {
+        return Box::new(
+            docker.images()
+                .get(tool_image_name)
+                .inspect()
+                .then(move |res| {
+                    match res {
+                        Ok(_) => futures::future::ok(()),
+                        Err(err) => {
+                            match &err {
+                                shiplift::errors::Error::Fault{code, message: _} if *code == 404 => eprintln!("Could not find {} locally. Quitting!", tool_image_name),
+                                _  => eprintln!("{}", err)
+                            };
+                            futures::future::err(())
+                        }
+                    }
+                })
+            );
+    } else {
+        let pull_options = PullOptions::builder()
+            .image(tool_image_name)
+            .build();
+
+        return Box::new(
+            docker.images()
+                .pull(&pull_options)
+                .inspect(|item| println!("{}", item["status"].as_str().unwrap()))
+                .collect()
+                .then(move |res| {
+                    match res {
+                        Ok(_) => {
+                            futures::future::ok(())
+                        },
+                        Err(err) => {
+                            match &err {
+                                shiplift::errors::Error::Fault{code, message: _} if *code == 404 => eprintln!("Could not find {} on Docker Hub. Quitting!", tool_image_name),
+                                _  => eprintln!("{}", err)
+                            };
+                            futures::future::err(())
+                        }
+                    }
+                })
+            );
+    }
 }
