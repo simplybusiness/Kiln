5ff3d57059f2e82854185335f9398af280fe6b4a
Daniel Murphy <daniel.murphy@simplybusiness.co.uk>
2019-12-17 12:15:52 +0000

    Bundler-audit: Update entrypoint script to cleanup tool output text file after it is not needed, fixed #106.

--- tool-images/ruby/bundler-audit/entrypoint.sh
+++ tool-images/ruby/bundler-audit/entrypoint.sh
@@ -4,3 +4,5 @@ bundle audit check --update | tee tool-output.txt
 END_TIME=$(date -I'seconds' | sed "s/\(.*\)\(.\{2\}\)$/\1:\2/")
 
 /data-forwarder --tool-name=bundler-audit --tool-version=0.6.1 --tool-output-path=tool-output.txt --start-time="$START_TIME" --end-time="$END_TIME" --output-format=PlainText --scan-env="$SCAN_ENV" --app-name="$APP_NAME" --endpoint-url="$DATA_COLLECTOR_URL"
+
+rm tool-output.txt
