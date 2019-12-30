18dae1b956b7df535708af92ff1cb865ea4ec08d
Daniel Murphy <daniel.murphy@simplybusiness.co.uk>
2019-12-30 16:40:51 +0000

    Report-parser: Connects to Kafka and deserialises Avro encoded ToolReports and prints to console

--- report-parser/src/main.rs
+++ report-parser/src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use avro_rs::Reader;
+use failure::err_msg;
+use kiln_lib::kafka::*;
+use kiln_lib::tool_report::ToolReport;
+use std::convert::TryFrom;
+use std::env;
+
+fn main() -> Result<(), std::boxed::Box<dyn std::error::Error>> {
+    let config = get_bootstrap_config(&mut env::vars())
+        .map_err(|err| failure::err_msg(format!("Configuration Error: {}", err)))?;
+
+    let ssl_connector = build_ssl_connector().map_err(|err| {
+        failure::err_msg(format!(
+            "OpenSSL Error {}: {}",
+            err.errors()[0].code(),
+            err.errors()[0].reason().unwrap()
+        ))
+    })?;
+
+    let mut consumer = build_kafka_consumer(
+        config,
+        "ToolReports".to_string(),
+        "report-parser".to_string(),
+        ssl_connector,
+    )
+        .map_err(|err| err_msg(format!("Kafka Consumer Error: {}", err.description())))?;
+
+
+    loop {
+        for ms in consumer.poll().unwrap().iter() {
+            for m in ms.messages() {
+                let reader = Reader::new(m.value)?;
+                for value in reader {
+                    let report = ToolReport::try_from(value?)?;
+                    println!("{:?}", report);
+                }
+            }
+            consumer.consume_messageset(ms)?;
+        }
+        consumer.commit_consumed()?;
+    }
 }
