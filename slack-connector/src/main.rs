cddb6f700b0f8c31834db7462dce1497db9195ee
Daniel Murphy <daniel.murphy@simplybusiness.co.uk>
2020-02-21 12:44:01 +0000

    Slack-connector: Switch to using rdkafka

--- slack-connector/src/main.rs
+++ slack-connector/src/main.rs
@@ -1,7 +1,10 @@
 use avro_rs::Reader;
 use failure::err_msg;
+use futures_util::stream::StreamExt;
 use kiln_lib::kafka::*;
 use kiln_lib::dependency_event::DependencyEvent;
+use rdkafka::consumer::{CommitMode, Consumer};
+use rdkafka::message::Message;
 use reqwest::blocking::Client;
 use reqwest::Method;
 use serde_json::{json, Value};
@@ -10,37 +13,30 @@ use std::env;
 use std::error::Error;
 use std::convert::TryFrom;
 
-fn main() -> Result<(), Box<dyn Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     env_logger::init();
-    openssl_probe::init_ssl_cert_env_vars();
 
     let oauth_token = env::var("OAUTH2_TOKEN").expect("Error: Required Env Var OAUTH2_TOKEN not provided");
     let channel_id = env::var("SLACK_CHANNEL_ID").expect("Error: Required Env Var SLACK_CHANNEL_ID not provided");
     let config = get_bootstrap_config(&mut env::vars())
         .map_err(|err| failure::err_msg(format!("Configuration Error: {}", err)))?;
 
-    let ssl_connector = build_ssl_connector().map_err(|err| {
-        failure::err_msg(format!(
-            "OpenSSL Error {}: {}",
-            err.errors()[0].code(),
-            err.errors()[0].reason().unwrap()
-        ))
-    })?;
-
-    let mut consumer = build_kafka_consumer(
+    let consumer = build_kafka_consumer(
         config.clone(),
-        "DependencyEvents".to_string(),
         "slack-connector".to_string(),
-        ssl_connector.clone(),
     )
         .map_err(|err| err_msg(format!("Kafka Consumer Error: {}", err.description())))?;
+
+    consumer.subscribe(&vec!["DependencyEvents"])?;
+    let mut messages = consumer.start_with(std::time::Duration::from_secs(1), false);
 
     let client = Client::new();
 
     loop {
-        for ms in consumer.poll().unwrap().iter() {
-            for m in ms.messages() {
-                let reader = Reader::new(m.value)?;
+        if let Some(Ok(message)) = messages.next().await {
+            if let Some(body) = message.payload() {
+                let reader = Reader::new(body)?;
                 for value in reader {
                     let event = DependencyEvent::try_from(value?)?;
                     let payload = json!({
@@ -59,9 +55,8 @@ fn main() -> Result<(), Box<dyn Error>> {
                     }
                 }
             }
-            consumer.consume_messageset(ms)?;
+            consumer.commit_message(&message, CommitMode::Async)?;
         }
-        consumer.commit_consumed()?;
     }
 }
 
