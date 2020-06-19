ad5d4843192f637ecd72375c44cbc62a0dbb7ea6
Daniel Murphy <daniel.murphy@simplybusiness.co.uk>
2020-06-19 17:02:04 +0100

    Slack-connector: Test certs are present in /tls before attempting to copy them in entrypoint.sh

--- slack-connector/entrypoint.sh
+++ slack-connector/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-cp /tls/* /usr/local/share/ca-certificates
-/usr/sbin/update-ca-certificates
+if [[ -d /tls ]]; then
+    if find /tls/ -mindepth 1 | read; then
+        cp /tls/* /usr/local/share/ca-certificates
+        /usr/sbin/update-ca-certificates
+    fi
+fi
 gosu kilnapp:kilngroup /app/slack-connector
