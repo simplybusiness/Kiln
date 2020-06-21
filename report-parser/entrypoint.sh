e1aee7ab382283ba1302f7eb92e6f85f10f0592a
Dan Murphy <daniel.murphy@simplybusiness.co.uk>
2020-06-21 20:46:21 +0100

    Report-parser: Ensure both Alpine trust stores are updated in entrypoint script

--- report-parser/entrypoint.sh
+++ report-parser/entrypoint.sh
@@ -3,6 +3,7 @@ if [[ -d /tls ]]; then
     if find /tls/ -mindepth 1 | read; then
         cp /tls/* /usr/local/share/ca-certificates
         /usr/sbin/update-ca-certificates
+        cp /etc/ssl/certs/ca-certificates.crt /etc/ssl/cert.pem
     fi
 fi
 gosu kilnapp:kilngroup /app/report-parser
