f25685d898fcf7aaa5611ec99e42f657843ddee6
Daniel Murphy <daniel.murphy@simplybusiness.co.uk>
2020-06-23 19:35:52 +0100

    Bundler-audit: Use unique file name in bundler-audit tool run

--- tool-images/ruby/bundler-audit/entrypoint.sh
+++ tool-images/ruby/bundler-audit/entrypoint.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 START_TIME=$(date -I'seconds' | sed "s/\(.*\)\(.\{2\}\)$/\1:\2/")
+FILE_NAME=$(mktemp -t ./tool-output)
 if [[ $OFFLINE == true ]]; then
-    bundle audit check | tee tool-output.txt
+    bundle audit check | tee $FILE_NAME
 else
-    bundle audit check --update | tee tool-output.txt 
+    bundle audit check --update | tee $FILE_NAME
 fi
 END_TIME=$(date -I'seconds' | sed "s/\(.*\)\(.\{2\}\)$/\1:\2/")
 
-/data-forwarder --tool-name=bundler-audit --tool-version=0.6.1 --tool-output-path=tool-output.txt --start-time="$START_TIME" --end-time="$END_TIME" --output-format=PlainText --scan-env="$SCAN_ENV" --app-name="$APP_NAME" --endpoint-url="$DATA_COLLECTOR_URL"
+/data-forwarder --tool-name=bundler-audit --tool-version=0.6.1 --tool-output-path="$FILE_NAME" --start-time="$START_TIME" --end-time="$END_TIME" --output-format=PlainText --scan-env="$SCAN_ENV" --app-name="$APP_NAME" --endpoint-url="$DATA_COLLECTOR_URL"
 
-rm tool-output.txt
+rm $FILE_NAME 
