3fadfb1b099929fd85c7f8d99e0992b02859e109
Divya Muthukumaran <Divya.Muthukumaran@simplybusiness.co.uk>
2021-06-10 12:37:23 +0100

    Yarn-audit: Tweaks to the entrypoint file

--- tool-images/javascript/yarn-audit/entrypoint.sh
+++ tool-images/javascript/yarn-audit/entrypoint.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
+FILE_NAME=$(mktemp -t -p . tool-output.XXXXXXXXXX)
 START_TIME=$(date -I'seconds' | sed "s/\(.*\)\(.\{2\}\)$/\1:\2/")
 YARN_VERSION=$(yarn audit --version)
-yarn audit --json | tee $FILE_NAME
+yarn audit --json --no-progress | tee $FILE_NAME 
 END_TIME=$(date -I'seconds' | sed "s/\(.*\)\(.\{2\}\)$/\1:\2/")
-
-/data-forwarder --tool-name=yarn-audit --tool-version="$YARN_VERSION" --tool-output-path="$FILE_NAME" --start-time="$START_TIME" --end-time="$END_TIME" --output-format=PlainText --scan-env="$SCAN_ENV" --app-name="$APP_NAME" --endpoint-url="$DATA_COLLECTOR_URL"
-
-rm $FILE_NAME 
+/data-forwarder --tool-name=yarn-audit --tool-version="0.1.1" --tool-output-path="$FILE_NAME" --start-time="$START_TIME" --end-time="$END_TIME" --output-format=JSON --scan-env="$SCAN_ENV" --app-name="$APP_NAME" --endpoint-url="$DATA_COLLECTOR_URL"
+rm $FILE_NAME
