8ec804d3b04222e20b5b9597df950ebb37531b83
Divya Muthukumaran <Divya.Muthukumaran@simplybusiness.co.uk>
2021-07-05 12:25:07 +0100

    Yarn-audit: Use tool version from command

--- tool-images/javascript/yarn-audit/entrypoint.sh
+++ tool-images/javascript/yarn-audit/entrypoint.sh
@@ -4,5 +4,5 @@ START_TIME=$(date -I'seconds' | sed "s/\(.*\)\(.\{2\}\)$/\1:\2/")
 YARN_VERSION=$(yarn audit --version)
 yarn audit --json --no-progress | tee $FILE_NAME 
 END_TIME=$(date -I'seconds' | sed "s/\(.*\)\(.\{2\}\)$/\1:\2/")
-/data-forwarder --tool-name=yarn-audit --tool-version="0.1.1" --tool-output-path="$FILE_NAME" --start-time="$START_TIME" --end-time="$END_TIME" --output-format=JSON --scan-env="$SCAN_ENV" --app-name="$APP_NAME" --endpoint-url="$DATA_COLLECTOR_URL"
+/data-forwarder --tool-name=yarn-audit --tool-version="$YARN_VERSION" --tool-output-path="$FILE_NAME" --start-time="$START_TIME" --end-time="$END_TIME" --output-format=JSON --scan-env="$SCAN_ENV" --app-name="$APP_NAME" --endpoint-url="$DATA_COLLECTOR_URL"
 rm $FILE_NAME
